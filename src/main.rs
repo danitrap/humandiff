be232ea47cf14fa199823f5498d94627d2c03961
Daniele Trapani <daniele.trapani@outlook.it>
2023-12-15 14:01:07 +0100

    Refactor main.rs to improve error handling and clarity

--- src/main.rs
+++ src/main.rs
@@ -5,35 +5,62 @@ use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 use std::{env, vec};
 
-fn explain_diff(explanation: &str, suggested_commit: &str) {
-    println!("Concise explanation: {}", explanation);
-    println!("Suggested commit message: {}", suggested_commit);
-}
-
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    if !std::path::Path::new(".git").exists() {
-        eprintln!("Not in a git repo");
-        return Ok(());
-    }
+    let git_diff = get_git_diff()?;
 
-    let output = std::process::Command::new("git").arg("diff").output()?;
-
-    if !output.status.success() {
-        eprintln!("Failed to execute git diff");
-        return Ok(());
-    }
-
-    let git_diff = String::from_utf8_lossy(&output.stdout);
-
-    // check if git_diff is empty
-    if git_diff.trim().is_empty() {
-        eprintln!("No changes were made");
-        return Ok(());
-    }
+    let prompt = format!(
+        r#"Can you help me explain to my team what changes were made in this repo?
+        Git Diff: {}"#,
+        git_diff
+    );
 
     let api_token = env::var("OPENAI_API_KEY")?;
     let client = Client::new(api_token);
 
+    let req = ChatCompletionRequest::new(GPT4.to_string(), vec![user_message(prompt)])
+        .functions(build_openai_functions())
+        .function_call(FunctionCallType::Auto);
+
+    // INFO: debug what the request looks like
+    // let serialized = serde_json::to_string(&req).unwrap();
+    // println!("{}", serialized);
+
+    let result = client.chat_completion(req)?;
+
+    process_chat_completion_response(result)?;
+
+    Ok(())
+}
+
+fn get_git_diff() -> Result<String, Box<dyn std::error::Error>> {
+    if !std::path::Path::new(".git").exists() {
+        return Err("Not in a git repo".into());
+    }
+
+    let output = std::process::Command::new("git").arg("diff").output()?;
+    if !output.status.success() {
+        return Err("Failed to execute git diff".into());
+    }
+
+    let git_diff = String::from_utf8_lossy(&output.stdout);
+
+    if git_diff.trim().is_empty() {
+        return Err("No changes were made".into());
+    }
+
+    Ok(git_diff.to_string())
+}
+
+fn user_message(content: String) -> chat_completion::ChatCompletionMessage {
+    chat_completion::ChatCompletionMessage {
+        role: chat_completion::MessageRole::user,
+        content,
+        function_call: None,
+        name: None,
+    }
+}
+
+fn build_openai_functions() -> Vec<chat_completion::Function> {
     let mut properties = HashMap::new();
     properties.insert(
         "explanation".to_string(),
@@ -59,22 +86,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             items: None,
         }),
     );
-
-    let prompt = format!(
-        r#"What changes were made in this diff?
-        Git Diff: {}"#,
-        git_diff
-    );
-    let req = ChatCompletionRequest::new(
-        GPT4.to_string(),
-        vec![chat_completion::ChatCompletionMessage {
-            role: chat_completion::MessageRole::user,
-            content: prompt,
-            function_call: None,
-            name: None,
-        }],
-    )
-    .functions(vec![chat_completion::Function {
+    let functions = vec![chat_completion::Function {
         name: String::from("explain_diff"),
         description: Some(String::from("Explains the diff to the user")),
         parameters: chat_completion::FunctionParameters {
@@ -85,27 +97,15 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 String::from("suggested_commit"),
             ]),
         },
-    }])
-    .function_call(FunctionCallType::Auto);
+    }];
+    functions
+}
 
-    // INFO: debug what the request looks like
-    // let serialized = serde_json::to_string(&req).unwrap();
-    // println!("{}", serialized);
-
-    let result = client.chat_completion(req)?;
-
+fn process_chat_completion_response(
+    result: chat_completion::ChatCompletionResponse,
+) -> Result<(), Box<dyn std::error::Error>> {
     match result.choices[0].finish_reason {
-        None => {
-            eprintln!("No finish_reason");
-            eprintln!("{:?}", result.choices[0].message.content);
-        }
-        Some(chat_completion::FinishReason::stop) => {
-            eprintln!("Stop");
-            eprintln!("{:?}", result.choices[0].message.content);
-        }
-        Some(chat_completion::FinishReason::length) => {
-            eprintln!("Length");
-        }
+        None => Err("OpenAI returned no finish reason".into()),
         Some(chat_completion::FinishReason::function_call) => {
             #[derive(Serialize, Deserialize)]
             struct DiffExplained {
@@ -121,14 +121,13 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             if name == "explain_diff" {
                 explain_diff(&explanation, &suggested_commit);
             }
+            Ok(())
         }
-        Some(chat_completion::FinishReason::content_filter) => {
-            eprintln!("ContentFilter");
-        }
-        Some(chat_completion::FinishReason::null) => {
-            eprintln!("Null");
-        }
+        Some(_) => Err("OpenAI returned an unexpected finish reason".into()),
     }
+}
 
-    Ok(())
+fn explain_diff(explanation: &str, suggested_commit: &str) {
+    println!("Concise explanation: {}", explanation);
+    println!("Suggested commit message: {}", suggested_commit);
 }
